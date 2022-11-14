b2cbf89539c29cb139cb472b81f39f17cacd8b40
samrob90 <bukarishamsu8@gmail.com>
2022-11-14 17:21:05 +0000

    frontend

---
+++ frontend/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.views.generic.base import TemplateView
+from django.views.generic.list import ListView
+
+
+class HomeVIew(TemplateView):
+    template_name = "frontend/home.html"
+
+
+# change to list view when model is ready
+class ShopView(TemplateView):
+    template_name = "frontend/shop.html"
+    # model = None
+    # paginate_by = 100
