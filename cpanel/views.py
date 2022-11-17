ddf164633a152c9252703c164da4da01386d2623
samrob90 <bukarishamsu8@gmail.com>
2022-11-17 14:06:10 +0000

    17/11/2022

--- cpanel/views.py
+++ cpanel/views.py
@@ -4,6 +4,13 @@ from . import form
 from django.contrib.auth import authenticate, login, logout
 from authentications.models import Account
 from django.contrib import messages
+from django.http import HttpResponse
+from . import models
+from PIL import Image
+from random import Random, random
+from bookstore.settings import BASE_DIR
+import uuid, os, json
+from django.template.defaultfilters import slugify
 
 # Create your views here.
 
@@ -26,8 +33,7 @@ class CpanelLoginView(TemplateView):
             """
             username = form.cleaned_data.get("username")
             password = form.cleaned_data.get("password")
-            print(username)
-            print(password)
+
             # print(Account.objects.get(username))
 
             email = Account.objects.filter(username=username).first()
@@ -67,6 +73,77 @@ class CpanelAddbookView(TemplateView):
 
     def get(self, request, *args, **kwargs):
         return render(request, self.template_name, {"form": self.class_form})
+
+    def post(self, request, *args, **kwargs):
+
+        if request.POST:
+            default_price = 0
+            default_type = ""
+            booktype = []
+            booktype_sample = ["PAPERBACK", "EBOOK", "AUDIOBOOK", "HARDCOVER"]
+
+            # hard coded to be changed later
+            product_name = "book"
+            product_type = "retail"
+
+            booktitle = request.POST["title"]
+            author = request.POST["author"]
+            quantity = request.POST["quantity"]
+            slug = slugify(booktitle)
+            images = request.FILES.getlist("files")
+
+            for index, value in enumerate(request.POST):
+                if value in booktype_sample:
+                    booktype.append({value: request.POST[value]})
+
+            if booktype:
+                default_price = booktype[0][1]
+                default_type = booktype[0][0]
+
+            # create product object
+            product = models.product.objects.create(
+                product_name=product_name, product_type=product_type
+            )
+            thumbnail_url = []
+            # create images thumbnail
+            for index, image in enumerate(images):
+                path = os.path.join(BASE_DIR, "media/thumbnail/")
+                thumbnail = thumbnail(image, path)
+                thumbnail_url.append(thumbnail)
+
+            # create book object
+            book = models.book.objects.create(
+                product=product,
+                title=booktitle,
+                quantity=quantity,
+                author=author,
+                slug=slug,
+                default_price=default_price,
+                default_type=default_type,
+                thumbnail=thumbnail_url[0],
+            )
+            # insert image
+            thumbnail_image_url = " ".join(thumbnail_url)
+            for i in image:
+                models.bookimages.objects.create(
+                    book=book,
+                    thumbnail=thumbnail_image_url,
+                    images=i,
+                )
+
+            return HttpResponse("done")
+
+    def thumbnail(self, image, path):
+        images = Image.open(image)
+        MAX_SIZE = (150, 200)
+        images.thumbnail(MAX_SIZE)
+        image_name = f"{uuid.uuid4()}thumbnail.webp"
+
+        full_name = f"{path}/{image_name}"
+        # imag
+        images.save(full_name)
+        # images.show()
+        return image_name
 
 
 class CpanelLogoutVIew(TemplateView):
--- static/cpanel/assets/js/book.js
+++ static/cpanel/assets/js/book.js
@@ -1,5 +1,15 @@
 $(document).ready(function () {
+    const csftoken = Cookies.get('csrftoken');
+    // const csftoken = $("input[name='csrfmiddlewaretoken']").val()
+    $.ajaxSetup({
+        beforeSend: function (xhr, settings) {
+            if (!(/^http:.*/.test(settings.url))) {
+                xhr.setRequestHeader("X-CSRFToken", csftoken)
+            }
+        }
+    })
     let ImageUrl = ""
+    // first step continue clicked
     $(".continue").click(function (e) {
         e.preventDefault();
         $(".preview").removeClass("d-none")
@@ -7,8 +17,8 @@ $(document).ready(function () {
         $(".first_").hide()
 
         $("#second").show()
-
-        let data = new FormData()
+        let files = $("#files")[0].files
+        let data = new FormData($("#form").get(0))
         let title = $("#id_title").val()
         let author = $("#id_author").val()
         let quantity = $("#id_quantity").val()
@@ -18,23 +28,28 @@ $(document).ready(function () {
         data.append("title", title)
         data.append("author", author)
         data.append("quantity", quantity)
+        data.append("files", files)
 
         addDetails("Title :", title)
         addDetails("Author :", author)
         addDetails("Quantity :", quantity)
+        readFile($("#files")[0].files[0])
         $("#outputimage").attr("src", ImageUrl)
 
-
+        // add book type 
+        let number = 0
         $(".addbooktype").click(function (e) {
             e.preventDefault();
+
             let booktype = $("#id_booktype").val()
             let booktypeprice = $("#id_booktypeprice").val()
             let booktypedescription = $("#description").val()
 
-            data.append("booktype", booktype)
-            data.append("booktype_price", booktypeprice)
-            data.append("booktype_description", booktypedescription)
+            let booktype_data = [booktype, booktypeprice, booktypedescription]
+            // booktype_.push(booktype_data)
+            // let name = "booktype_" + number
 
+            data.append(booktype, booktype_data)
 
             if (booktypedescription != "" || booktypeprice != "") {
                 addDetails(booktype, booktypeprice + " GHS")
@@ -45,7 +60,42 @@ $(document).ready(function () {
             $("#id_booktype").val(" ")
             $("#id_booktypeprice").val(" ")
             $("#description").val(" ")
+            number++
 
+        });
+
+
+        // previous click
+        $(".previous").click(function (e) {
+            e.preventDefault();
+            $(".second").hide()
+            $(".first_").show()
+            $("#continue").removeClass(" d-none")
+            $(".first_value_added").remove()
+            $(".second_value_added").remove()
+        });
+
+
+        // next click
+        $(".next").click(function (e) {
+            e.preventDefault();
+            // data.append("booktype", booktype_)
+
+            $.ajax({
+                url: "/cpanel/add-book",
+                type: "POST",
+                data: data,
+                dataType: 'json',
+                cache: false,
+                processData: false,
+                contentType: false,
+                success: function (data, textStatus, jqXHR) {
+                    alert(data)
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                    //if fails     
+                }
+            });
         });
 
     });
@@ -53,19 +103,18 @@ $(document).ready(function () {
 
 
     function addDetails(title, value) {
-        let html = "<li><strong> " + title + "</strong> <span>" + value + "</span></li>"
+        let html = "<li class='first_value_added'><strong> " + title + "</strong> <span>" + value + "</span></li>"
         $("#details_ul").append(html)
-
     }
 
     function add_booktype_in_space(booktype, booktypeprice) {
-        let html_ = "<div class='alert alert-info' role='alert'>" + booktype + " | " + booktypeprice + " GHS" + " </div>"
+        let html_ = "<div class='alert alert-info second_value_added' role='alert'>" + booktype + " | " + booktypeprice + " GHS" + " </div>"
         $("#booktypecover").append(html_)
     }
 
     $("#files").change(function (e) {
         e.preventDefault();
-        readFile(e)
+        readFile($("#files")[0].files[0])
 
     });
     // previeew file
@@ -77,7 +126,10 @@ $(document).ready(function () {
 
             ImageUrl = reader.result
 
+
         };
-        reader.readAsDataURL(event.target.files[0]);
+        reader.readAsDataURL(event);
+        // reader.readAsDataURL(event);
+
     };
 });
