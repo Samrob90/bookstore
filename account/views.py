9a08de90b44b3bbff388a4faf96d8f8a01905ee8
samrob90 <bukarishamsu8@gmail.com>
2023-03-04 10:21:23 +0000

    s

--- account/views.py
+++ account/views.py
@@ -3,12 +3,10 @@ from django.views.generic.base import TemplateView
 from django.views.generic.list import ListView
 from frontend.models import wishlist as frontend_wishlist
 from cpanel.models import order, Addresse
+from frontend.models import wishlist, cart
 from django.contrib import messages
 from django.http import HttpResponse, JsonResponse
-
-
 from django.views.generic.detail import DetailView
-
 
 # Create your views here.
 class AccountHomeView(TemplateView):
@@ -83,3 +81,39 @@ class WishlistView(ListView):
     ordering = ["-created_at"]
     template_name: str = "frontend/account/wishlist.html"
     context_object_name = "wishlist"
+
+    def post(self, request, *args, **kwargs):
+        if "user_account_add_to_cart" in request.POST:
+            wishlist_pk = request.POST.get("wishlistpk")
+            # fetch wishlist data from wishlist model
+            wishlist_object = wishlist.objects.filter(
+                pk=wishlist_pk, user=request.user
+            ).first()
+            # seach in cart is this book is already in cart
+            book_cart = cart.objects.filter(
+                product_id=wishlist_object.product_id, user=request.user
+            )
+            if book_cart.exists():
+                messages.error(request, "This book is already in your cart ")
+                return redirect("wishlist")
+            # add cart new record
+            cart.objects.create(user=request.user, **cart_format(wishlist_object))
+            wishlist_object.delete()
+
+            messages.success(
+                request, f"{wishlist_object.booktitle} added to cart successfully !"
+            )
+            return redirect("wishlist")
+
+
+def cart_format(book):
+    return {
+        "product_id": str(book.product_id),
+        "booktitle": book.booktitle,
+        "bookquantity": 1,
+        "bookthumbnail": book.bookthumbnail,
+        "bookprice": float(book.bookprice),
+        "booktype": book.booktype,
+        "bookauthor": book.bookauthor,
+        "bookslug": book.bookslug,
+    }
